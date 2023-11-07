2e936e4ec4609079859f844df0aa445015f1cb0f
bangkit <bangkitputrakristana@gmail.com>
2023-11-07 19:38:33 +0700

    add delete data queries

--- Query/hapus_data.sql
+++ Query/hapus_data.sql
@@ -1,3 +1,18 @@
--- Hapus data detail pesanan dengan id 1
-DELETE FROM detail_pesanan
-WHERE id = 1;
+-- Hapus data detail pesanan dengan id 7
+DELETE FROM online_retail.detail_pesanan
+WHERE id = 7;
+
+-- Hapus data pesanan dengan id 7
+DELETE FROM online_retail.pesanan
+WHERE id = 7;
+
+-- Hapus data barang dengan id 7
+DELETE FROM online_retail.barang
+WHERE id = 7;
+
+-- Hapus data pelanggan dengan id 7
+DELETE FROM online_retail.pelanggan
+WHERE id = 7;
+
+
+
