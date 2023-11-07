acde4e331cb3da174339e0d5264cf0999d405a85
bangkit <bangkitputrakristana@gmail.com>
2023-11-07 14:51:56 +0700

    add hapus_tabel

---
+++ hapus_tabel.sql
@@ -0,0 +1,14 @@
+-- Hapus tabel kategori_barang
+DROP TABLE kategori_barang;
+
+-- Hapus tabel barang
+DROP TABLE barang;
+
+-- Hapus tabel pelanggan
+DROP TABLE pelanggan;
+
+-- Hapus tabel pesanan
+DROP TABLE pesanan;
+
+-- Hapus tabel detail_pesanan
+DROP TABLE detail_pesanan;
