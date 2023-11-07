342bd0322ea5dd23563100449511f7319a95b791
bangkit <bangkitputrakristana@gmail.com>
2023-11-07 14:15:25 +0700

    add get_data sql file

---
+++ get_data.sql
@@ -0,0 +1,14 @@
+-- Mengambil data dari tabel barang
+SELECT * FROM online_retail.barang;
+
+-- Mengambil data dari tabel pelanggan
+SELECT * FROM online_retail.pelanggan;
+
+-- Mengambil data dari tabel kategori_barang
+SELECT * FROM online_retail.kategori_barang;
+
+-- Mengambil data dari tabel pesanan
+SELECT * FROM online_retail.pesanan;
+
+-- Mengambil data dari tabel detail_pesanan
+SELECT * FROM online_retail.detail_pesanan;
